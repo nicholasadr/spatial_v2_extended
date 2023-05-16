04d18c9124a971f92e6e118185ff7bf880e4b647
nicholasadr <adr.nicholas@gmail.com>
2023-05-16 12:04:19 -0400

    Use casadi fn for IK: qd->y

--- models/tello_hip_differential.m
+++ models/tello_hip_differential.m
@@ -69,24 +69,8 @@ I{4} = mcI( inertial_4.mass{1}, ...
             inertial_4.Ic{1});
 
 % inverse kinematics of hip differential: qd -> y
-% qd(1) = hip roll
-% qd(2) = hip pitch
-% y(1) = actuator 2
-% y(2) = actuator 3
-a1 = 57/2500 - (8*cos(qd(1))*sin(qd(2)))/625 - (7*sin(qd(1)))/625;
-b1 = -(8*cos(qd(2)))/625;
-c1 = (49*cos(qd(1)))/5000 + (399*sin(qd(1)))/20000 + ...
-    (57*cos(qd(1))*sin(qd(2)))/2500 - (7*sin(qd(1))*sin(qd(2)))/625 - 3021/160000;
-d1 = sqrt(a1^2 + b1^2 - c1^2);
-a2 = (7*sin(qd(1)))/625 - (8*cos(qd(1))*sin(qd(2)))/625 + 57/2500;
-b2 = -(8*cos(qd(2)))/625;
-c2 = (49*cos(qd(1)))/5000 - (399*sin(qd(1)))/20000 + ...
-    (57*cos(qd(1))*sin(qd(2)))/2500 + (7*sin(qd(1))*sin(qd(2)))/625 - 3021/160000;
-d2 = sqrt(a2^2 + b2^2 - c2^2);
-
-y(1) = atan2(-b1*d1 + a1*c1, b1*c1+a1*d1);
-y(2) = atan2(-b2*d2 + a2*c2, b2*c2+a2*d2);
-
+y_fn = external('IK_dependent_state_to_y', './models/casadi_fn/y_gen.so');
+y = full(y_fn(qd));
 q = [y(1) y(2) qd(1) qd(2)]';
 
 for i = 1:NB
@@ -232,6 +216,14 @@ crfv(19:24,19:24) = crf(v_(19:24));
 %f_ = I_*a_ + crf(v_)*I_*v_;
 f_ = I_*a_ + crfv*I_*v_;
 tau_ = S' * f_;
+
+%% debug
+%G3 = [A B];
+%G3_dot = [A_dot B_dot];
+%a1_ = S_single_joint{3}*G3_dot*y_dot; % [-0.2114; 0;0;0;0;0]
+%a2_ = S_ring(13:18,:)*y_dot; % [-0.2114; 0;0;0;0;0]
+%a3_ = G3_dot * y_dot; % -0.2114
+%a4_ = g(3); % 0.1075
 
 robot.NB = NB;
 robot.parent = parent;
